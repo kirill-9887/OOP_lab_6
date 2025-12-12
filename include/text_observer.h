bb2f9f10779b9be724f832cd2dc7f92ac81a2c2c
kirill-9887 <>
2025-12-12 15:15:38 +0000

    Text observer commit 2

--- include/text_observer.h
+++ include/text_observer.h
@@ -9,6 +9,11 @@ private:
     TextObserver(){};
 
 public:
+    TextObserver(const TextObserver&) = delete;
+    TextObserver(TextObserver&&) noexcept = delete;
+    TextObserver& operator=(const TextObserver&) = delete;
+    TextObserver& operator=(TextObserver&&) noexcept = delete;
+
     static std::shared_ptr<IFightObserver> get();
 
     void on_fight(
